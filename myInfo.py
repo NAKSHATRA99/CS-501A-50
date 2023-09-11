68e37a46150a9d5a65d80c574b82cc878a2d2e12
NAKSHATRA99 <144635657+NAKSHATRA99@users.noreply.github.com>
2023-09-10 20:33:11 -0400

    my information v2

--- myInfo.py
+++ myInfo.py
@@ -1,11 +1,15 @@
-# CS 501A-50
-
-#Your first name and Last name
-
-#myInfo
-print("nakshatra nitin kawthale")
+Python 3.11.5 (v3.11.5:cce6ba91b3, Aug 24 2023, 10:50:31) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # CS 501A-50
+... 
+... #Your first name and Last name
+... 
+... #myInfo
+>>> print("nakshatra nitin kawthale")
 nakshatra nitin kawthale
-print("23 campbell, deal,NJ, 07786, USA")
+>>> print("23 campbell, deal,NJ, 07786, USA")
 23 campbell, deal,NJ, 07786, USA
-print("+1 888-344-5436")
+>>> print("+1 888-344-5436")
 +1 888-344-5436
+>>> print("My MS program is imformation system technology track")
+My MS program is imformation system technology track
