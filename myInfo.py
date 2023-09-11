5cc86ec5bfab17aa0ccb87aea31cb1d051adfde9
NAKSHATRA99 <144635657+NAKSHATRA99@users.noreply.github.com>
2023-09-10 20:21:46 -0400

    my information v1

---
+++ myInfo.py
@@ -0,0 +1,11 @@
+# CS 501A-50
+
+#Your first name and Last name
+
+#myInfo
+print("nakshatra nitin kawthale")
+nakshatra nitin kawthale
+print("23 campbell, deal,NJ, 07786, USA")
+23 campbell, deal,NJ, 07786, USA
+print("+1 888-344-5436")
++1 888-344-5436
