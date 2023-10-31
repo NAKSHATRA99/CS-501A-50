f681fe78c23dddc1c9e82a3eea050ddf47170378
NAKSHATRA99 <144635657+NAKSHATRA99@users.noreply.github.com>
2023-10-31 15:41:35 -0400

    average from input .py

--- average from input .py
+++ average from input .py
@@ -1,21 +1,30 @@
+#CS 175/501A
+#Nakshatra Nitin Kawthale
+# average from input
 
+def main():
+    infile = open('numbers.txt', 'r')
+    file_contents = infile.read()
+    infile.close()
+    print(file_contents)
 
-
-
-
-
-with open('numbers.txt', 'r') as file:
-    numbers = []
+    num_list = []
     total = 0
     count = 0
-    for line in file:
-        num = float(line.strip())
-        count += 1
-        total += num
-        numbers.append(number)
-        print('I read in', count, 'number(s) Current number is:', number, 'Total is:', total)
 
+    lines = file_contents.split('\n')
+    for line in lines:
+        try:
+            num = float(line.strip())
+            count += 1
+            total += num
+            num_list.append(num)
+            print('I read in', count, 'number(s). Current number is:', num, 'Total is:', total)
+        except ValueError:
+            pass
 
-average = total / count
-print('Average: ', average)
+    average = total / count if count > 0 else 0
+    print('Average:', average)
 
+if __name__ == '__main__':
+    main()
